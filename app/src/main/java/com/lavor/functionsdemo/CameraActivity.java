8ed435b808072fd9552325f744ff2dfc405c1e94
flyliufu <flyliufu@sina.com>
2017-03-06 11:03:04 +0800

    Result page change

--- app/src/main/java/com/lavor/functionsdemo/CameraActivity.java
+++ app/src/main/java/com/lavor/functionsdemo/CameraActivity.java
@@ -8,6 +8,7 @@ import android.graphics.ImageFormat;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.graphics.YuvImage;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.hardware.Camera;
@@ -20,7 +21,6 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Display;
-import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -29,6 +29,7 @@ import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
 import com.googlecode.tesseract.android.TessBaseAPI;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -47,7 +48,6 @@ public class CameraActivity extends AppCompatActivity
   private Display display;
   private RectImageView mIvPhotoRect;
   private byte[] mData;
-  private Bitmap mBitmap;
   //识别语言英文
   static final String DEFAULT_LANGUAGE = "eng";
   private Point mPoint;
@@ -55,17 +55,25 @@ public class CameraActivity extends AppCompatActivity
   private boolean isFinish = true;
   private Camera.PreviewCallback mPreviewCallback = new Camera.PreviewCallback() {
     @Override public void onPreviewFrame(byte[] data, Camera camera) {
-
-      if (isFinish) {
+      if (isFinish && !isPause && isOk) {
+        isOk = false;
+        Log.d(TAG, "onPreviewFrame: ");
         isFinish = false;
-        mData = new byte[data.length];
-        System.arraycopy(data, 0, mData, 0, mData.length);
+        mData = data;
         exeTask();
       }
     }
   };
   private Rect mDrawRect;
   private boolean isPause;
+  private boolean isOk;
+
+  private Handler mHandler = new Handler() {
+    @Override public void handleMessage(Message msg) {
+      isOk = true;
+      mHandler.sendEmptyMessageDelayed(0, 1500);
+    }
+  };
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -135,6 +143,7 @@ public class CameraActivity extends AppCompatActivity
   }
 
   private void openCamera(SurfaceHolder holder) {
+    Log.d(TAG, "openCamera: ");
     try {
       int cameraIndex = findBackCamera();
       if (cameraIndex != -1) {
@@ -147,26 +156,18 @@ public class CameraActivity extends AppCompatActivity
 
   private void initCamera(SurfaceHolder holder, int cameraIndex) throws IOException {
     Log.d(TAG, "initCamera: ");
-    release();
-
     camera = Camera.open(cameraIndex);
-    // camera.setPreviewCallback(mPreviewCallback);
     camera.setPreviewDisplay(holder); // 设置用于显示拍照影像的SurfaceHolder对象
+    camera.setPreviewCallback(mPreviewCallback);
     camera.setDisplayOrientation(0); // 相机自然的角度
+    camera.startPreview();
   }
-
-  private Handler mHandler = new Handler() {
-    @Override public void handleMessage(Message msg) {
-      camera.takePicture(CameraActivity.this, null, CameraActivity.this);
-      mHandler.sendEmptyMessageDelayed(0, 1000);
-    }
-  };
 
   @Override protected void onPause() {
     Log.d(TAG, "onPause: ");
     super.onPause();
     isPause = true;
-    // mHandler.removeCallbacksAndMessages(null);
+    release();
   }
 
   public void autoFocus() {
@@ -232,13 +233,18 @@ public class CameraActivity extends AppCompatActivity
   @Override protected void onResume() {
     Log.d(TAG, "onResume: ");
     super.onResume();
+    isFinish = true;
     isPause = false;
+    mHandler.sendEmptyMessageDelayed(0, 1500);
   }
 
   /**
    * 设置照片格式
    */
   private void setParameter() {
+    Log.d(TAG, "setParameter: ");
+
+    if (camera == null) return;
     try {
       Camera.Parameters parameters = camera.getParameters(); // 获取各项参数
       parameters.setPictureFormat(ImageFormat.JPEG); // 设置图片格式
@@ -252,7 +258,7 @@ public class CameraActivity extends AppCompatActivity
       parameters.setPictureSize(optimalPreviewSize.width, optimalPreviewSize.height);
       camera.setParameters(parameters);
     } catch (Exception e) {
-
+      e.printStackTrace();
     }
   }
 
@@ -260,6 +266,8 @@ public class CameraActivity extends AppCompatActivity
    * 查找后置摄像头
    */
   private int findBackCamera() {
+    Log.d(TAG, "findBackCamera: ");
+
     int cameraCount = 0;
     Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
     cameraCount = Camera.getNumberOfCameras(); // get cameras number
@@ -277,32 +285,26 @@ public class CameraActivity extends AppCompatActivity
   @Override public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
     Log.d(TAG, "surfaceChanged: ");
     if (!isPause) {
-
       setParameter();
       camera.startPreview();
     }
-
-    // mHandler.sendEmptyMessageDelayed(0, 3000);
   }
 
   @Override public void surfaceDestroyed(SurfaceHolder holder) {
     Log.d(TAG, "surfaceDestroyed: ");
-    release();
   }
 
   @Override public void onClick(View v) {
-
-    camera.takePicture(this, null, this);
+    // camera.takePicture(this, null, this);
   }
 
   @Override public void onPictureTaken(byte[] data, Camera camera) {
     Log.d(TAG, "onPictureTaken: execute");
     camera.startPreview();
 
-    if (isFinish) {
+    if (isFinish && !isPause) {
       isFinish = false;
-      mData = new byte[data.length];
-      System.arraycopy(data, 0, mData, 0, mData.length);
+      mData = data;
       exeTask();
     }
   }
@@ -311,8 +313,11 @@ public class CameraActivity extends AppCompatActivity
   }
 
   private void exeTask() {
+    Log.d(TAG, "exeTask: ");
 
     new AsyncTask<Void, Void, Boolean>() {
+      private Bitmap mBitmap;
+
       @Override protected void onPreExecute() {
         //设置图片可以缓存
         mDrawRect = mIvPhotoRect.getDrawRect();
@@ -320,10 +325,27 @@ public class CameraActivity extends AppCompatActivity
       }
 
       @Override protected Boolean doInBackground(Void... params) {
+        Bitmap bitmap = null;
 
         Log.d(TAG, "doInBackground: " + mData);
         if (mData != null && mPoint != null && mDrawRect != null) {
-          Bitmap bitmap = BitmapFactory.decodeByteArray(mData, 0, mData.length);
+
+          try {
+            Camera.Parameters parameters = camera.getParameters();
+            int width = parameters.getPreviewSize().width;
+            int height = parameters.getPreviewSize().height;
+
+            YuvImage yuv = new YuvImage(mData, parameters.getPreviewFormat(), width, height, null);
+
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            yuv.compressToJpeg(new Rect(0, 0, width, height), 100, out);
+
+            byte[] bytes = out.toByteArray();
+            bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+          //Bitmap bitmap = BitmapFactory.decodeByteArray(mData, 0, mData.length);
           Log.d(TAG, "bitmap: " + bitmap);
           if (bitmap == null) return false;
           //截取
@@ -357,25 +379,23 @@ public class CameraActivity extends AppCompatActivity
       }
 
       @Override protected void onPostExecute(Boolean isSuccess) {
-        isFinish = true;
         Log.d(TAG, "onPostExecute: " + isSuccess);
         if (isSuccess) {
-          String s = check(exeEnglishOCR());
+          String s = check(exeEnglishOCR(mBitmap));
+          //Drawable drawable = new BitmapDrawable(getResources(), mBitmap);
+          //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+          //  mIvResult.setBackground(drawable);
+          //} else {
+          //  mIvResult.setBackgroundDrawable(drawable);
+          //}
+          mBitmap.recycle();
           if (s != null && (s.length() == 15 || s.length() == 18)) {
-            Drawable drawable = new BitmapDrawable(getResources(), mBitmap);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-              mIvResult.setBackground(drawable);
-            } else {
-              mIvResult.setBackgroundDrawable(drawable);
-            }
-
-            mHandler.removeCallbacksAndMessages(null);
-
             Intent intent = new Intent(CameraActivity.this, ResultActivity.class);
             intent.putExtra("result", s);
             startActivity(intent);
           } else {
-            Toast.makeText(getBaseContext(), "识别有误，请重新拍照", Toast.LENGTH_SHORT).show();
+            isFinish = true;
+            // Toast.makeText(getBaseContext(), "识别有误，请重新拍照", Toast.LENGTH_SHORT).show();
           }
         }
       }
@@ -383,6 +403,9 @@ public class CameraActivity extends AppCompatActivity
   }
 
   private String check(String str) {
+    Log.d(TAG, "check: " + str);
+    if (str == null) return "";
+
     return str.replaceAll("\t", "")
         .replace(" ", "")
         .replace("\n", "")
@@ -400,7 +423,8 @@ public class CameraActivity extends AppCompatActivity
 
   String regEx = "[^0-9A-Za-z]";
 
-  public String exeEnglishOCR() {
+  public String exeEnglishOCR(Bitmap bitmap) {
+    Log.d(TAG, "exeEnglishOCR: ");
     //获取缓存的bitmap
     final TessBaseAPI baseApi = new TessBaseAPI();
     //初始化OCR的训练数据路径与语言
@@ -410,7 +434,7 @@ public class CameraActivity extends AppCompatActivity
     //设置识别模式
     baseApi.setPageSegMode(TessBaseAPI.PageSegMode.PSM_SINGLE_LINE);
     //设置要识别的图片
-    baseApi.setImage(mBitmap);
+    baseApi.setImage(bitmap);
     String utf8Text = baseApi.getUTF8Text();
     String[] split = utf8Text.split(" ");
     String result = null;
@@ -431,20 +455,14 @@ public class CameraActivity extends AppCompatActivity
   }
 
   @Override protected void onDestroy() {
-    Log.d(TAG, "onDestroy: ");
     super.onDestroy();
-    release();
-  }
 
-  @Override public boolean onKeyDown(int keyCode, KeyEvent event) {
-    if (KeyEvent.KEYCODE_BACK == keyCode) {
-      release();
-    }
-    return super.onKeyDown(keyCode, event);
+    mHandler.removeCallbacksAndMessages(null);
   }
 
   private void release() {
     if (camera != null) {
+      Log.d(TAG, "release: success");
       camera.stopPreview();
       camera.release(); // 释放照相机
       camera = null;
